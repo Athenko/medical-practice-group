563f3adaf0cf538c25c66b6af06b9f9aa952d04e
Athenkosi Bika <s226193926@mandela.ac.za>
2025-10-12 17:03:24 +0200

    Initial commit - React app setup

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+         <h1>Welcome to My Medical Practice App!</h1>
+        <p>
+          This is where are our web page activities will be.
+        </p>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
